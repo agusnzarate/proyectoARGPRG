5ffd685e55552dba6d1e9e31ace37f2a256fd84b
Agustin Zarate <agusnzarate@gmail.com>
2022-06-24 08:04:02 -0300

    Componente de Proyectos
    Creada la parte de proyectos
     y algunos cambios en la parte de Skills

--- proyectoARGPRG/src/app/app.module.ts
+++ proyectoARGPRG/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
